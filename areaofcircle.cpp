bb66a4b0695524fa95d0cb226924b3ad5d1b1e15
GARAKOTI <rahulsinghgarakoti730@gmail.com>
2022-08-06 18:40:17 +0530

    add a progaram to find the area of circle

---
+++ areaofcircle.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+   double area;
+   double radius;
+   cout<<"Enter the radius of a circle"<<endl;
+   cin>>radius;
+   area = 3.14 * radius* radius;
+   cout<<"The area of circle is :-  "<<area<<endl; 
+}
