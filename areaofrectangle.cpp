99448a474610dbbc233ada0098fa0d6769f65313
GARAKOTI <rahulsinghgarakoti730@gmail.com>
2022-08-06 19:15:41 +0530

    Area of Rectangle

---
+++ areaofrectangle.cpp
@@ -0,0 +1,19 @@
+//C++ program for Area of Rectangle
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    double area;
+    double L;
+    double W;
+    cout<<"Enter the lenth of Rectangle"<<endl;
+    cin>>L;
+    cout<<"Enter the width of Rectangle"<<endl;
+    cin>>W;
+    area = L * W;
+    cout<<"The area of Rectangle is :-  "<<area<<endl;
+
+
+    return 0;
+
+}
