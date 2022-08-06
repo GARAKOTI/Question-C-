f0f85f3c1cb1c8ef50e368c9f3a39bb2eadd1464
GARAKOTI <rahulsinghgarakoti730@gmail.com>
2022-08-06 19:07:12 +0530

    Area of triangle

---
+++ areaoftriangle.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    double area ;
+    double b;
+    double h;
+    cout<<"Enter the base of Triangle"<<endl;
+    cin>>b;
+    cout<<"Enter the hight of Triangle"<<endl;
+    cin>>h;
+    area = 0.5 *b *h;
+    cout<<"The area of Triangle is : -  "<<area<<endl;
+    
+}
