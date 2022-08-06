439e19429ad3af883e261f1be7b91369f6064992
GARAKOTI <rahulsinghgarakoti730@gmail.com>
2022-08-06 18:57:53 +0530

    Area of square

---
+++ areaofsquare.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    float area;
+    float side;
+    cout<<"Enter the side of square"<<endl;
+    cin>>side;
+    area = side * side;
+    cout<<"The area of circle:-  "<<area<<endl;
+}
