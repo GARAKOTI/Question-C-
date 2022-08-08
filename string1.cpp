e6132b87a7339c1a7603b1364b8ffe924cc1c611
GARAKOTI <rahulsinghgarakoti730@gmail.com>
2022-08-08 22:57:18 +0530

    strating with string in C++

---
+++ string1.cpp
@@ -0,0 +1,11 @@
+// C++ programe for concatenate the two strings
+#include<iostream>
+#include<string.h>
+using namespace std;
+int main(){
+    string str1 = "Rahul  ";
+    string str2 = "Singh Garakoti";
+    string result;
+    result = str1 + str2;
+    cout<<result<<endl;
+}
