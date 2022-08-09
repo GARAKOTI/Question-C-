5564877d4fb38a6d80dc0b0338af8e2acc32d528
GARAKOTI <rahulsinghgarakoti730@gmail.com>
2022-08-09 10:54:59 +0530

    To convert string to lowercase

---
+++ string3.cpp
@@ -0,0 +1,11 @@
+// C++ program to convert string to lower case
+#include<iostream>
+#include<string.h>
+using namespace std;
+int main(){
+    char str1[20];
+    cout<<"Enter the string to convert to lowercase :- "<<endl;
+    cin.getline(str1,20);
+    cout<<"Character in lower case :- "<<strlwr(str1)<<endl;
+
+    }
