526b5bfee94a742d2aa3e8ae7dc423a2ad53cb70
GARAKOTI <rahulsinghgarakoti730@gmail.com>
2022-08-09 10:59:43 +0530

    To convert a string lower case to uppercase

---
+++ string4.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+#include<string.h>
+using namespace std;
+int main(){
+    char str1[22];
+    cout<<"Enter the string to convert to uppercase:- "<<endl;
+    cin.getline(str1,22);
+    cout<<"Character in uppercase :- "<<strupr(str1)<<endl;
+
+    }
