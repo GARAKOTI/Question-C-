57c44faee519c653819612ec541b4cbb954c6491
GARAKOTI <rahulsinghgarakoti730@gmail.com>
2022-08-09 10:05:29 +0530

    To conecatenate to string

---
+++ string2.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<string.h>
+using namespace std;
+int main(){
+    char str1[50] ,str2[50] , result[100];
+     cout<<"Enter the first name :- "<<endl;
+     cin.getline(str1, 50);
+     cout<<"Enter the middle or last name :- "<<endl;
+     cin.getline(str2, 50);
+    strcat(str1 , str2);
+    cout<<"you full name is :- "<<str1<<endl;
+
+    }
