e3d8771dfae25b8df0e88dfbe39d0b7e7d15286d
GARAKOTI <rahulsinghgarakoti730@gmail.com>
2022-08-14 10:48:30 +0530

    Learning loops

---
+++ loops1.cpp
@@ -0,0 +1,13 @@
+// Write C++ program to print alphabets from a to z
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    char i;
+   cout<<"The alphabet A to Z :-"<<endl;
+   for(i='A'; i<='Z'; i++)
+   {
+    cout<<i<<endl;
+   }
+   return 0;
+}
---
+++ loops2.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    char c;
+    for(c='A'; c<='Z'; c++ ){
+        cout<<"ASCII Value of entered charchter :-"<<c<<"="<<(int)c<<endl;
+    }
+}
